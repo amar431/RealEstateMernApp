7bd954e750de974725a80f4c86cb8b1c507afbc5
BATCHU AMARNATH GUPTHA <batchuamarnathgupta1@gmail.com>
2023-11-02 16:14:15 +0530

    MongoDb connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Mongodb is Connected")
+}).catch((err)=>{
+    console.log(`MongoDb error is ${err}`)
+})
 const app = express()
 
 app.listen(3000,()=>{
